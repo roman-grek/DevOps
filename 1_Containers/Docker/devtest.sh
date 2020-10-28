d4c5e3beb6ccb961cfe4c54b77bffa084a9cc0cf
Roman Grek <roman.grek0098@gmail.com>
2020-10-28 15:25:25 +0300

    Fix nginx reverse proxy

--- 1_Containers/Docker/devtest.sh
+++ 1_Containers/Docker/devtest.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo docker run -d -it --name devtest --mount type=bind,source="$(pwd)"/nginx/etc,target=/nginx/etc -p 8080:80 nginx
+docker run -d -it --name devtest --mount type=bind,source="$(pwd)"/nginx/etc,target=/nginx/etc -p 8080:80 nginx
